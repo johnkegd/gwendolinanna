f30bc24a9e2d1357f7825a3d7c7e7d36e4d5b739
John Garcia <john.garcia@ajila.com>
2021-04-11 00:29:50 +0200

    materialize card component example added, image added, testing row in home page

---
+++ src/components/materialize/Row.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Card from './Card.svelte';
+    export let elements; 
+</script>
+<div class="row">
+    {#each elements as element}
+    <div class="col s4">
+        {#if element === "card"}
+        <Card/>
+        {:else}
+        {element} none
+        {/if}
+    </div>
+    {/each}
+</div>
+        
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,13 @@
 <script>
+	import Card from '../components/materialize/Card.svelte';
+	import cardImage from 'images/example-500x500.jpg';
+	let Cards = [
+		{title:"hi card 0", description:"hi description 0"},
+		{title:"hi card 1", description:"hi description 1", image:cardImage},
+		{title:"hi card 2", description:"hi description 2"},
+		{title:"hi card 3", description:"hi description 3", image:cardImage},
+		{title:"hi card 4", description:"hi description 4"},
+	]
 </script>
 
 <style>
@@ -15,3 +24,12 @@
 
 <h1>Home  Page!</h1>
 <p><strong>here is going to be the landing page.</strong></p>
+<div class="row">
+	{#each Cards as card}
+	<div class="col s4">
+		<Card {...card}/>
+	</div>
+{/each}
+</div>
+
+
