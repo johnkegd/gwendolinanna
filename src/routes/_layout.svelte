42ba956bae0b516e586c019467a57d55e423f533
John Garcia <john.garcia@ajila.com>
2021-05-08 14:13:22 +0200

    testing navBar component for landing, not needed js commented in template

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import NavbarAuthor from '../components/Navbars/AuthNavbar.svelte';
-	import Nav from '../components/Nav.svelte';
 //	import Footer from '../components/Footer.svelte';
 import Footer from '../components/Footers/Footer.svelte';
 	export let segment;
