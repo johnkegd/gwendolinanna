e5e795292d726f64d1e374853be3d96e7c1b483a
Manuel Duran <manuelmid99@gmail.com>
2021-05-20 22:07:33 +0200

    New 3dCard

--- src/routes/test/index.svelte
+++ src/routes/test/index.svelte
@@ -1,10 +1,12 @@
 <script>
     import Pepito from '../../components/Cards/CardPromo.svelte';
     import Color from '../../components/Color.svelte';
+    import Card3D from '../../components/Cards/Card3DEfect.svelte';
 </script>
 
 
 
-<Pepito/>
-<Color color="yellow"/>
+<!-- <Pepito/> -->
+<!-- <Color color="green"/> -->
 
+<Card3D/>
