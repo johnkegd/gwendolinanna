1af8cb5c1ca571a84bb8fc0878dd717c83762da8
John Garcia <john.garcia@ajila.com>
2021-05-31 01:04:32 +0200

    login-page: fireworks animation via svelte component

--- src/components/GraphicArts/FireWorks.js
+++ src/components/GraphicArts/FireWorks.js
@@ -1,31 +1,31 @@
 export function fire(){
     var t = 0;
-    var c = document.querySelector("canvas");     
-    var $ = c.getContext('2d');
-    c.width = window.innerWidth - 800;
-    c.height = window.innerHeight - 800;
-    $.fillStyle = 'hsla(0,0%,0%,1)';
+    var canvas = document.querySelector("canvas");     
+    var ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth - 800;
+    canvas.height = window.innerHeight - 800;
+    ctx.fillStyle = 'hsla(0,0%,0%,1)';
 
     window.addEventListener('resize', function() {
-      c.width = window.innerWidth;
-      c.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
     }, false);
 
     function draw() {
-      $.globalCompositeOperation = 'source-over';
-      $.fillStyle = 'hsla(0,0%,0%,.1)';
-      $.fillRect(0, 0, c.width, c.height);
+      ctx.globalCompositeOperation = 'source-over';
+      ctx.fillStyle = 'hsla(0,0%,0%,.1)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
       var foo, i, j, r;
       foo = Math.sin(t) * 2 * Math.PI;
       for (i=0; i<400; ++i) {
         r = 400 * Math.sin(i * foo);
-        $.globalCompositeOperation = '';
-        $.fillStyle = 'hsla(' + i + 12 + ',100%, 60%,1)';
-        $.beginPath();
-        $.arc(Math.sin(i) * r + (c.width / 2), 
-              Math.cos(i) * r + (c.height / 2), 
+        ctx.globalCompositeOperation = '';
+        ctx.fillStyle = 'hsla(' + i + 12 + ',100%, 60%,1)';
+        ctx.beginPath();
+        ctx.arc(Math.sin(i) * r + (canvas.width / 2), 
+              Math.cos(i) * r + (canvas.height / 2), 
               1.5, 0, Math.PI * 2);
-        $.fill();
+              ctx.fill();
 
       }
       t += 0.000005;
